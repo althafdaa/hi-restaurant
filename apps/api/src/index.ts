e1de74b808bf78d6720fd020bb4379396eb36cdf
althafdemiandra <althafdemiandraakbar@gmail.com>
2024-12-02 17:21:14 +0700

    chore: try

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.json({ message: "Hello, World!" });
+  return c.json({ message: "Hello, new changes!" });
 });
 
 export default app;
